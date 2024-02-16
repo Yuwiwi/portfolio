33f8b42b1300ceb9426e2e3d290f223cb0973925
Yuri Rubio <yurirubio10@gmail.com>
2024-02-17 02:44:20 +0800

    new

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-function toggleMenu() {
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon");
-    menu.classList.toggle("open")
-    icon.classList.toggle("open")
-}
+ScrollReveal({
+    reset:true,
+    distance: '60px',
+    duration: 2500,
+    delay: 400
+});
+
+ScrollReveal().reveal('.main-title, .section-title', {delay: 500, origin: 'left'});
+ScrollReveal().reveal('.content-2', {delay: 700, origin: 'left'});
+ScrollReveal().reveal('.content-1', {delay: 900, origin: 'left'});
